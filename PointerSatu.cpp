675d76b4ced859aa54b643eac3de1a2d3f8d1339
hudangrh <hudangrhkuliah@gmail.com>
2026-04-27 15:17:26 +0700

    PointerSatu Int Main

--- PointerSatu.cpp
+++ PointerSatu.cpp
@@ -10,3 +10,17 @@ class mahasiswa
     }
 };
 
+int main ()
+{
+    mahasiswa Mhs{1}; // object mhs;
+    Mhs.showNim(); //Member Acces operator
+    
+    mahasiswa &refMhs = Mhs; // pointer reference reffmhs
+    refMhs.Nim = 2; // member acces operator
+    Mhs.showNim();
+
+    mahasiswa *pMhs = &Mhs; // pointer deference pMhs
+    pMhs->Nim = 3; // arrow operator
+    pMhs->showNim();
+    return 0;
+}
