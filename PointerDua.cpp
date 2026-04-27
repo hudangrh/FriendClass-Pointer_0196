dc291e54775eb80b252f3bc710d5ebbaac264013
hudangrh <hudangrhkuliah@gmail.com>
2026-04-27 15:04:10 +0700

    Class Mahasiswa

---
+++ PointerDua.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa
+{
+public:
+    int Nim;
+    void showNim()
+    {
+        cout << "No Induk = " << Nim << endl;
+    }
+};
+
+int main()
+{
+    mahasiswa *mhs = new mahasiswa[1]; // pointer object mhs
+    mhs->Nim = 2;
+    mhs->showNim();
+    delete mhs;
+    return 0;
+}
